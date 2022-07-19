b5975bab24d38c58abdbbfb72bd5275af4ecc261
borecjeborec1 <robingrundel@seznam.cz>
2022-07-19 20:55:22 +0200

    removed codesGeneration

--- helpers/generateCodes.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs")
-let data = fs.readFileSync("helpers/codes.txt", "utf8")
-let obj = {}
-data.split("\r\n").forEach(el => obj[el.trim().split(" ")[0]] = +el.trim().split(" ")[1])
-console.log(obj)
